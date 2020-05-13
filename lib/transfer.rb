4b725090b3c4af74a26f90583efa6d20c22a02a4
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-13 19:44:47 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,25 +14,24 @@ class Transfer
  end
  
  def execute_transaction
-  if self.valid? && self.status = "pending"
+  if self.valid? && self.status == "pending" && sender.balance > amount
       sender.balance -= amount
       receiver.deposit(amount)
       self.status = "complete"
   else 
-      return "Transaction rejected. Please check your account balance."
       self.status = "rejected"
+      return "Transaction rejected. Please check your account balance."
   end
  end
  
  def reverse_transfer
-   if execute_transaction && self.status = "complete"
+   if self.valid? && self.status == "complete" && receiver.balance > amount
      receiver.balance -= amount
      sender.deposit(amount)
      self.status = "reversed"
     end
   end
 
- 
- 
+
  
 end
