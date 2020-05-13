b125fd1adc672e92c55bbd33687b44d6b845be6d
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-13 23:58:35 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,12 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
+  
+  def initialize(account_name)
+    @name = account_name
+    @balance = 1000
+    @status = "open"
+  end 
+  
 
 end
